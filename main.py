63230e6dc62e62db05c8d5b6de8261174c13ab57
Andrew Jessup <ajessup@gmail.com>
2011-11-23 08:55:28 +1100

    Bit 'o cleanup

--- main.py
+++ main.py
@@ -1,75 +1,64 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import os, cgi, logging, datetime, md5
 
 from django.utils import simplejson as json
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template, util
 from google.appengine.ext import db
 
 from models import Path
 
 class IndexHandler(webapp.RequestHandler):
-    def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, None))
+	"""
+	Render viewport to be shown on a projector
+	"""
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+		self.response.out.write(template.render(path, None))
 
 class DrawHandler(webapp.RequestHandler):
-    def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'templates/draw.html')
-        self.response.out.write(template.render(path, None))
+	"""
+	Render drawpad (to be shown on an iPhone)
+	"""
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/draw.html')
+		self.response.out.write(template.render(path, None))
 
 class DataHandler(webapp.RequestHandler):
-    def post(self):
-        logging.info("Data POST Starting...")
-        data = json.loads(self.request.body)   
-        paths = json.dumps(data['paths'])
-        if len(paths) > 400:
-            json_hash = md5.new(str(paths)[400]).hexdigest()
-        else:
-            json_hash = md5.new(str(paths)).hexdigest()
-        logging.info("Storing: %s - %s" % (json_hash, paths))
-        stored_path = Path(key_name = json_hash, json=paths)
-        stored_path.put()
-        self.response.out.write('{ "success" : "ok" }')   
+	"""
+	Handle saving and retreiving drawings
+	"""
+	def post(self):
+		data = json.loads(self.request.body)
+		try:
+			paths = json.dumps(data['paths'])
+			key_name = md5.new(str(paths)).hexdigest()
+			Path(key_name = key_name, json=paths).put()
+			self.response.out.write('{ "success" : "ok" }')	 
+		except Exception, e:
+			self.response.out.write('{ "error" : %s }' % str(e))	 
  
-    def get(self):
-        drawing_id = self.request.get('drawing_id', default_value=None)
-        logging.info("Data GET Starting (%s)..." % drawing_id)
-        if drawing_id is None:
-            q = Path.all()
-            q.order("play_count")
-            path = q.fetch(1)[0]
-        else:
-            path = Path.get(db.Key(drawing_id))
-        path.play_count = path.play_count + 1
-        path.put()
-        self.response.out.write('[{"paths":'+path.json+',"success":true}]')
+	def get(self):
+		drawing_id = self.request.get('drawing_id', default_value=None)
+		if drawing_id is None:
+			q = Path.all().order("play_count")
+			if q.count():
+				path = q.fetch(1)[0]
+			else:
+				path = Path(key_name='initial',json='{}')
+		else:
+			path = Path.get(db.Key(drawing_id))
+		path.play_count += 1
+		path.put()
+		self.response.out.write('[{"paths":%s,"success":true}]' % path.json)
 
 def main():
-    application = webapp.WSGIApplication([('/', IndexHandler),
-                                          ('/draw', DrawHandler),
-                                          ('/data.json', DataHandler),
-                                         ],
-                                         debug=True)
-    util.run_wsgi_app(application)
+	application = webapp.WSGIApplication([('/', IndexHandler),
+										  ('/draw', DrawHandler),
+										  ('/data.json', DataHandler),
+										 ],
+										 debug=True)
+	util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- models.py
+++ models.py
@@ -5,40 +5,44 @@ from StringIO import StringIO
 from django.utils import simplejson as json
 
 class Path(db.Model):
-    json = db.TextProperty(required=True)
-    date_created = db.DateTimeProperty(required=True, auto_now_add=True)
-    play_count = db.IntegerProperty(required=True, default=0)
+	"""
+	Path model represents a series of points that make up a drawpad drawing
+	"""
+	
+	json = db.TextProperty(required=True)
+	date_created = db.DateTimeProperty(required=True, auto_now_add=True)
+	play_count = db.IntegerProperty(required=True, default=0)
 
-    def get_paths(self):
-        return json.loads(self.json)
+	PREVIEW_ZOOM_FACTOR = 0.25
 
-    def generate_preview_png(self):
-        paths = self.get_paths()
-    
-        max_x = 0
-        max_y = 0
+	def get_paths(self):
+		return json.loads(self.json)
 
-        zoom_factor = 0.25
+	def generate_preview_png(self):
+		paths = self.get_paths()
+		max_x = max_y = 0
 
-        # Determine bounds
-        for path in paths:
-            for point in path:
-               if point['x'] > max_x:
-                  max_x = point['x']
-               if point['y'] > max_y:
-                  max_y = point['y']
+		# Determine bounds
+		for path in paths:
+			for point in path:
+			   if point['x'] > max_x:
+				  max_x = point['x']
+			   if point['y'] > max_y:
+				  max_y = point['y']
 
-        max_x = int(max_x * zoom_factor)
-        max_y = int(max_y * zoom_factor)
+		max_x = int(max_x * self.PREVIEW_ZOOM_FACTOR)
+		max_y = int(max_y * self.PREVIEW_ZOOM_FACTOR)
 
-        img_matrix = [[1 for col in range(max_x+1)] for row in range(max_y+1)]
-        
-        for path in paths:
-            for point in path:
-               img_matrix[int(point['y']*zoom_factor)][int(point['x']*zoom_factor)] = 0       
-    
-        f = StringIO()
-        w = png.Writer(len(img_matrix[0]), len(img_matrix), greyscale=True, bitdepth=1)
-        w.write(f, img_matrix)
-        
-        return f.getvalue()
+		img_matrix = [[1 for col in range(max_x+1)] for row in range(max_y+1)]
+		
+		# Create matrix represzenting each point
+		for path in paths:
+			for point in path:
+			   img_matrix[int(point['y']*zoom_factor)][int(point['x']*zoom_factor)] = 0	
+				  
+		# Convert matrix to PNG
+		f = StringIO()
+		w = png.Writer(len(img_matrix[0]), len(img_matrix), greyscale=True, bitdepth=1)
+		w.write(f, img_matrix)
+		
+		return f.getvalue()
